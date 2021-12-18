a3c08b9193fa529d73acad3f871d4ecbb3d1a602
htmlHxcker <clintonadeleke@gmail.com>
2021-12-18 23:17:56 +0000

    [BREAKING_CHANGE]: Delete inject-script.js

--- src/scripts/inject-script.js
+++
@@ -1,7 +0,0 @@
-const FOREGROUND_ENTRY_POINT = document.createElement('div');
-let reactJS_script = document.createElement('script');
-
-FOREGROUND_ENTRY_POINT.id = 'foreground';
-FOREGROUND_ENTRY_POINT.appendChild(reactJS_script);
-
-document.querySelector('body').appendChild(FOREGROUND_ENTRY_POINT);
