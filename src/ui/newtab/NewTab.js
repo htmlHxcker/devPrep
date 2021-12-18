2a7e39a9caa6a7f02603f4949f4bb14a0573227d
htmlHxcker <clintonadeleke@gmail.com>
2021-12-18 22:58:52 +0000

    [FEAT]: Style username and time

--- src/ui/newtab/NewTab.js
+++ src/ui/newtab/NewTab.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
+import './newtab.css';
 
 const NewTab = () => {
 	return (
-		<div>
-			<h1>NewTab</h1>
-			<p>Hello World</p>
+		<div className="container container--newtab">
+			<div className="flex justify-content-sb">
+				<div>
+					<h2 className="ff-cardo fs-900">Hello, User Name</h2>
+				</div>
+				<div className="text-right">
+					<h2 className="fs-900">10:11AM</h2>
+					<p>Monday, 1 January 2022</p>
+				</div>
+			</div>
 		</div>
 	);
 };
