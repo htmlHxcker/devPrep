a8e8af6bca5d5cdb8a40209ebe0f72a835598938
htmlHxcker <clintonadeleke@gmail.com>
2021-12-18 23:03:16 +0000

    [FEAT]: Make time and date dynamic

---
+++ public/scripts/currentTime.js
@@ -0,0 +1,23 @@
+export default function currentTime() {
+	let date = new Date();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+	let session = 'AM';
+
+	if (hours === 0) {
+		hours = 12;
+	}
+	if (hours > 12) {
+		hours = hours - 12;
+		session = 'PM';
+	}
+
+	hours = hours < 10 ? '0' + hours : hours;
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	seconds = seconds < 10 ? '0' + seconds : seconds;
+
+	let time = hours + ':' + minutes + ':' + seconds + ' ' + session;
+
+	return time;
+}
--- src/ui/newtab/NewTab.js
+++ src/ui/newtab/NewTab.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
+
+import currentTime from '../../../public/scripts/currentTime';
+import '../../styles/index.css';
+import '../../styles/utilities.css';
 import './newtab.css';
 
 const NewTab = () => {
+	const [time, setTime] = useState(currentTime());
+
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'long',
+		day: 'numeric',
+	};
+	const DATE = new Date().toLocaleDateString(undefined, options);
+	setInterval(() => {
+		setTime(currentTime());
+	}, 1000);
 	return (
 		<div className="container container--newtab">
 			<div className="flex justify-content-sb">
@@ -10,8 +26,8 @@ const NewTab = () => {
 					<h2 className="ff-cardo fs-900">Hello, User Name</h2>
 				</div>
 				<div className="text-right">
-					<h2 className="fs-900">10:11AM</h2>
-					<p>Monday, 1 January 2022</p>
+					<h2 className="fs-900">{time}</h2>
+					<p>{DATE}</p>
 				</div>
 			</div>
 		</div>
