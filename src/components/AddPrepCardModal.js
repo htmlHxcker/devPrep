9d8a0df5708a56917410fff74c251038f430b1bb
htmlHxcker <clintonadeleke@gmail.com>
2022-01-07 16:21:11 +0000

    [FIX]: Modal is now purely presentational

--- src/components/AddPrepCardModal.js
+++ src/components/AddPrepCardModal.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import useField from '../utils/useField';
 import AddPrepCard from './AddPrepCard';
 import './AddPrepCardModal.css';
 import '../styles/utilities.css';
-import { setStorage } from '../utils/storage';
 
 const AddPrepCardModal = ({ modalState, cardState }) => {
-	const cardFront = useField('text');
-	const cardBack = useField('text');
-
-	function addPrepCard() {
-		if (!cardFront.value || !cardBack.value) {
-			alert('You have to fill the form to create a card!');
-			return;
-		}
-		let updatedCards = [
-			...cardState.cards,
-			{
-				CARD_FRONT: cardFront.value,
-				CARD_BACK: cardBack.value,
-			},
-		];
-
-		setStorage({ cards: updatedCards }, cardState.setCards(updatedCards));
-		modalState.setShowModal(false);
-	}
 	return (
 		<div className={`modal flex ${modalState.showModal ? '' : 'hide-modal'}`}>
-			<AddPrepCard />
+			<AddPrepCard
+				cardState={cardState}
+				setShowModal={modalState.setShowModal}
+			/>
 			<button
 				onClick={() => {
 					modalState.setShowModal(false);
