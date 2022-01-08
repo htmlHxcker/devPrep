d08d1e49dc82e4339063f1eee38d8da84766f77e
htmlHxcker <clintonadeleke@gmail.com>
2022-01-08 13:23:03 +0000

    [FIX]: Refactor AddPrepCardModal to use Modal

--- src/components/AddPrepCardModal.js
+++ src/components/AddPrepCardModal.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import AddPrepCard from './AddPrepCard';
-import './AddPrepCardModal.css';
 import '../styles/utilities.css';
+import Modal from './Modal';
 
 const AddPrepCardModal = ({ modalState, cardState }) => {
 	return (
-		<div className={`modal flex ${modalState.showModal ? '' : 'hide-modal'}`}>
+		<Modal modalState={modalState}>
 			<AddPrepCard
 				cardState={cardState}
 				setShowModal={modalState.setShowModal}
 			/>
-			<button
-				onClick={() => {
-					modalState.setShowModal(false);
-				}}
-				className="primary-btn close-modal-btn"
-			>
-				Cancel
-			</button>
-		</div>
+		</Modal>
 	);
 };
 
