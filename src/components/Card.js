fd9d6ac09e55b5ec49a97ace3e6c91cc0d1eb780
htmlHxcker <clintonadeleke@gmail.com>
2022-01-09 20:42:11 +0000

    [FEAT]: Create onClick event for editing cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
 import '../styles/utilities.css';
 import { FiEdit2, FiTrash2 } from 'react-icons/fi';
+import Modal from './Modal';
+import EditPrepCard from './EditPrepCard';
 
 const Card = ({ card, isCardFlipped, flipCard }) => {
-	return (
-		<div
-			onClick={flipCard}
-			className="card bg-carousel-pink active-card flex text-center"
-		>
-			{isCardFlipped ? (
-				<div>
-					<div className="text-right flex modify-icons">
-						<span>
-							<FiEdit2 size={25} />
-						</span>
-						<span>
-							<FiTrash2 size={25} color="red" />
-						</span>
-					</div>
+	const [showModal, setShowModal] = useState(false);
 
-					<p>{card.CARD_BACK} </p>
-				</div>
-			) : (
-				<h2>{card.CARD_FRONT}</h2>
-			)}
-		</div>
+	return (
+		<>
+			<div
+				onClick={flipCard}
+				className="card bg-carousel-pink active-card flex text-center"
+			>
+				{isCardFlipped ? (
+					<div>
+						<div className="text-right flex modify-icons">
+							<span
+								onClick={() => {
+									setShowModal(true);
+								}}
+							>
+								<FiEdit2 size={25} />
+							</span>
+							<span>
+								<FiTrash2 size={25} color="red" />
+							</span>
+						</div>
+
+						<p>{card.CARD_BACK} </p>
+					</div>
+				) : (
+					<h2>{card.CARD_FRONT}</h2>
+				)}
+			</div>
+			<Modal modalState={{ showModal, setShowModal }}>
+				<EditPrepCard setShowModal={setShowModal} card={card} />
+			</Modal>
+		</>
 	);
 };
 
