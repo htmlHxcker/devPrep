6ea2b26d2a2a8c73a137e6234c80974082f78940
htmlHxcker <clintonadeleke@gmail.com>
2022-01-12 14:06:01 +0000

    [FEAT]: Add more cases to cardReducer

--- src/reducers/cardReducer.js
+++ src/reducers/cardReducer.js
@@ -1,12 +1,46 @@
-const cardReducer = async (state = [], action) => {
+import { getStorage, setStorage } from '../utils/storage';
+
+const cardReducer = (state = [], action) => {
+	let updatedCards;
 	switch (action.type) {
+		case 'INIT_CARDS':
+			return action.data;
 		case 'ADD_CARD':
 			return [...state, action.data];
 		case 'EDIT_CARD':
-			return state.map((card) =>
+			updatedCards = state.map((card) =>
 				card.id !== action.data.id ? card : action.data
 			);
+			setStorage({ cards: updatedCards });
+			return updatedCards;
 		case 'DELETE_CARD':
-			return state.filter((card) => card.id !== action.data.id);
+			updatedCards = state.filter((card) => card.id !== action.data.id);
+			setStorage({ cards: updatedCards });
+			return updatedCards;
+		default:
+			return state;
 	}
 };
+
+export const initializeCards = () => {
+	return async (dispatch) => {
+		const cardsObj = await getStorage('cards');
+		dispatch({
+			type: 'INIT_CARDS',
+			data: cardsObj.cards,
+		});
+	};
+};
+
+export const addCard = (card) => {
+	return async (dispatch) => {
+		const cardsObj = await getStorage('cards');
+		setStorage({ cards: [...cardsObj.cards, card] });
+		dispatch({
+			type: 'ADD_CARD',
+			data: card,
+		});
+	};
+};
+
+export default cardReducer;
