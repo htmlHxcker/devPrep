653d4a412eed64c258879581438f41b7c1e5f961
htmlHxcker <clintonadeleke@gmail.com>
2022-01-13 13:22:11 +0000

    [FIX]: Combine reducers for current and cards

--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import cardReducer from './reducers/cardReducer';
+import currentReducer from './reducers/currentReducer';
 
-const store = createStore(cardReducer, applyMiddleware(thunk));
+const reducer = combineReducers({
+	cards: cardReducer,
+	current: currentReducer,
+});
+const store = createStore(reducer, applyMiddleware(thunk));
 export default store;
