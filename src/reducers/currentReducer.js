ee3f1c19a029ca7ef336011d454016575d03dce1
htmlHxcker <clintonadeleke@gmail.com>
2022-01-13 13:21:12 +0000

    [FEAT]: Create redicer for currentState

---
+++ src/reducers/currentReducer.js
@@ -0,0 +1,39 @@
+export default (state = 0, { type, payload }) => {
+	switch (type) {
+		case 'INCREMENT':
+			return payload;
+		case 'DECREMENT':
+			return payload;
+		case 'GET_CURRENT':
+			return state;
+		default:
+			return state;
+	}
+};
+
+export const incrementCurrent = (current, length) => {
+	return async (dispatch) => {
+		const addOneToCurrent = current === length - 1 ? 0 : current + 1;
+		dispatch({
+			type: 'INCREMENT',
+			payload: addOneToCurrent,
+		});
+	};
+};
+
+export const decrementCurrent = (current, length) => {
+	return async (dispatch) => {
+		const removeOneFromCurrent = current === 0 ? length - 1 : current - 1;
+		dispatch({
+			type: 'DECREMENT',
+			payload: removeOneFromCurrent,
+		});
+	};
+};
+export const getCurrent = () => {
+	return (dispatch) => {
+		dispatch({
+			type: 'GET_CURRENT',
+		});
+	};
+};
