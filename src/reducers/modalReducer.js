8d112bced148bb2930509f0fec218159b3fc9d88
htmlHxcker <clintonadeleke@gmail.com>
2022-01-13 15:52:39 +0000

    [FEAT]: Fix repetition in modalReducer

--- src/reducers/modalReducer.js
+++ src/reducers/modalReducer.js
@@ -1,23 +1,17 @@
 const initialState = {
-	addCard: false,
-	editCard: false,
-	deleteCard: false,
+	ADD: false,
+	EDIT: false,
+	DELETE: false,
 };
 
-export default (state = initialState, { type }) => {
+export default (state = initialState, { type, payload }) => {
 	switch (type) {
-		case 'OPEN_ADD_CARD_MODAL':
-			return { ...state, addCard: true };
-		case 'CLOSE_ADD_CARD_MODAL':
-			return { ...state, addCard: false };
-		case 'OPEN_EDIT_CARD_MODAL':
-			return { ...state, editCard: true };
-		case 'CLOSE_EDIT_CARD_MODAL':
-			return { ...state, editCard: false };
-		case 'OPEN_DELETE_CARD_MODAL':
-			return { ...state, deleteCard: true };
-		case 'CLOSE_DELETE_CARD_MODAL':
-			return { ...state, deleteCard: false };
+		case 'ADD_CARD_MODAL':
+			return { ...state, ADD: payload };
+		case 'EDIT_CARD_MODAL':
+			return { ...state, EDIT: payload };
+		case 'DELETE_CARD_MODAL':
+			return { ...state, DELETE: payload };
 		default:
 			return state;
 	}
