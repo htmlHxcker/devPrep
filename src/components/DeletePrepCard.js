21d818f348f40de01f1ebb333d35113f9e76da75
htmlHxcker <clintonadeleke@gmail.com>
2022-01-14 13:48:36 +0000

    [FEAT]: Add content to DeletePrepCard

--- src/components/DeletePrepCard.js
+++ src/components/DeletePrepCard.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { AiOutlineWarning } from 'react-icons/ai';
 import '../styles/utilities.css';
 
 const DeletePrepCard = () => {
+	const cards = useSelector((state) => state.cards);
+	const current = useSelector((state) => state.current);
+
+	const card = cards[current];
+	const dispatch = useDispatch();
+
+	async function deletePrepCard() {
+		dispatch({ type: 'DELETE_CARD_MODAL', payload: false });
+	}
 	return (
-		<div className="center-text">
-			<h2 className="ff-600 fs-cardo">Warning</h2>
-			<p className="fs-400">Are you sure you want to delete</p>
-			<div>
-				<buttton className="primary-btn">Yes, Delete it</buttton>
-				<buttton className="primary-btn">No, keep it</buttton>
-			</div>
+		<div className="flex modal--container ">
+			<AiOutlineWarning size={70} color="red" />
+			<h2 className="fs-900 ff-cardo">Before you pull the trigger...</h2>
+			<p className="fs-600">
+				Are you sure you want to delete <em>{card.CARD_FRONT}</em>
+			</p>
+			<button onClick={deletePrepCard} className="primary-btn">
+				Yes, Delete it
+			</button>
 		</div>
 	);
 };
