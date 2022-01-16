4e27e8de61d67b839448ce4a34d587f7b1a43815
htmlHxcker <clintonadeleke@gmail.com>
2022-01-16 21:48:10 +0000

    [FEAT]: Action creators for settings reducer

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,12 +1,36 @@
-const initialState = {};
+import { getStorage, setStorage } from '../utils/storage';
 
-export default (state = initialState, { type, payload }) => {
+export default (state, { type, payload }) => {
 	switch (type) {
 		case 'GET_SETTINGS':
-			return state;
+			return payload;
 		case 'SET_SETTINGS':
 			return payload;
 		default:
 			return state;
 	}
+};
+export const getSettings = () => {
+	return async (dispatch) => {
+		const settingsObj = await getStorage('settings');
+		const initialSettings = {
+			import: [],
+			contextMenu: 'YES',
+			username: '',
+		};
+		dispatch({
+			type: 'GET_SETTINGS',
+			payload: !settingsObj.settings ? initialSettings : settingsObj.settings,
+		});
+	};
+};
+
+export const setSettings = (values) => {
+	return async (dispatch) => {
+		await setStorage({ settings: values });
+		dispatch({
+			type: 'SET_SETTINGS',
+			payload: values,
+		});
+	};
 };
