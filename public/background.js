227003c54d44bfb8416286e751b66dadedabd7c8
htmlHxcker <clintonadeleke@gmail.com>
2022-01-17 16:49:40 +0000

    [FIX]: Remove initialSettings from background.js
    This commit removes the initialSettings from the background script

--- public/background.js
+++ public/background.js
@@ -1,10 +1,4 @@
-let initialSettings = {
-	import: [],
-	username: '',
-};
-
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({ settings: initialSettings });
 	let optionsUrl = chrome.runtime.getURL('/options.html');
 	chrome.tabs.create({
 		url: optionsUrl,
