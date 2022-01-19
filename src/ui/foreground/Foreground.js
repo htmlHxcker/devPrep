c32d26763654ee903be24daf5902167f591fd294
htmlHxcker <clintonadeleke@gmail.com>
2022-01-19 19:22:10 +0000

    [BREAKINGCHANGE]: Delete Foreground

--- src/ui/foreground/Foreground.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-const Foreground = () => {
-	return <h1>Foreground</h1>;
-};
-
-export default Foreground;
-render(<Foreground />, document.getElementById('foreground'));
